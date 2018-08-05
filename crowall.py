0ec28a92cc1a2935fb65dd35d0bb5b89a6f657de
cplayer-2018 <39280173+cplayer-2018@users.noreply.github.com>
2018-08-05 20:21:34 +0800

    support Python3

--- crowall.py
+++ crowall.py
@@ -1,47 +1,35 @@
 # coding:utf-8
-import sys
-
-reload(sys)
-sys.setdefaultencoding('utf8')
-#    __author__ = '郭 璞'
-#    __date__ = '2016/10/24'
-#    __Desc__ = 翻墙助手， 默认会先备份一下当前的hosts文件，防止出现意外，另外可以跨平台使用
-
-import platform
-import os
-import urllib2
+import requests as re,os,platform
 
 def downloadHosts(url):
-    file = open('./hosts.txt', 'wb')
-    data = urllib2.urlopen(url).readlines()
+    file = open('.\hosts.txt', 'wb')
+    data = re.get(url)
     file.writelines(data)
     file.close()
-
-
 
 def crosswall(systemtype='Window'):
     try:
         if systemtype == 'Windows':
-            os.system('copy %SystemRoot%\System32\drivers\etc\hosts  %SystemRoot%\System32\drivers\etc\hosts_bak')
+            os.system('copy %SystemRoot%\System32\drivers\etc\hosts %SystemRoot%\System32\drivers\etc\hosts_bak')
             os.system('copy hosts.txt %SystemRoot%\System32\drivers\etc\hosts')
             os.system('ipconfig /flushdns')
             os.system('pause')
-            print 'It\'s done on Windows! And Try your browser!'
+			print('It\'s done on Window! And Try your browser!')
         elif systemtype == "Linux":
-            os.system('cp /etc/hosts /etc/hosts_bak')
-            os.system('mv ./hosts.txt /etc/hosts')
+            os.system('cp /etc/hosts.txt /etc/hosts_bak')
+            os.system('mv ./hosts /etc/hosts')
             os.system('pause')
             os.system('sudo /etc/init.d/networking restart ')
-            print 'It\'s done on Linux! And Try your browser!'
+			print('It\'s done on Linux! And Try your browser!')
+        elif systemtype == "Darwin":
+            os.system('sudo cp /etc/hosts /etc/host_bak')
+            os.system('sudo mv ./hosts.txt /etc/hosts')
+            os.system('sudo ifconfig en0 down && sudo ifconfig en0 up')
+			print('It\'s done on Mac! And Try your browser!')
     except Exception as e:
-        print e
+        print(e)
 
-
-
-if __name__ == '__main__':
-
-    url = 'https://raw.githubusercontent.com/racaljk/hosts/master/hosts'
-    downloadHosts(url=url)
-    print 'Hosts update success!'
-    crosswall(platform.system())
-    print 'Hosts replaced success! Try to cross the wall!'
+url = 'https://raw.githubusercontent.com/googlehosts/hosts/master/hosts-files/hosts'
+downloadHosts(url=url)
+crosswall(platform.system())
+print('Hosts replaced success! Try to cross the wall!')
